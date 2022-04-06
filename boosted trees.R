2b715ef97c93d11612fc8bced1af3c0bccbdbcc2
samuelburge <sam.burge.15@gmail.com>
2022-04-06 14:16:33 -0700

    Corrected boosted trees
    Code was crashing R because the labels have to be numeric instead of factors for the function(s) to work correctly.

--- boosted trees.R
+++ boosted trees.R
@@ -1,38 +1,55 @@
 # Load the necessary packages
-require(MASS)
-require(class)
-require(tree)
-require(tidyverse)
-require(randomForest)
-require(gbm)
+library(MASS)
+library(class)
+library(tree)
+library(randomForest)
+library(gbm)
 
 # Set working directory and import the data file
 setwd("C:\\Users\\SamBu\\Desktop\\STAT 639")
 load("class_data.RData")
-dataset <- data.frame(y = as.factor(y), x)
+dataset <- data.frame(y, x) # Do not convert y to factor; crashed the gbm() function
 
 # Partition the data set into training and test sets
 split <- sample(c(TRUE,FALSE), nrow(dataset), replace = TRUE, prob = c(0.8, 0.2))
 train_set <- dataset[split, ]
 test_set <- dataset[!split, ]
 
-# Fit gradient-boosted trees to the data
-boost.fit <- gbm(y ~ ., data = train_set,
+# Fit boosted trees to the data with shrinkage = 0.01
+boost_fit <- gbm(y ~ ., data = train_set,
                  distribution = "bernoulli",
-                 n.trees = 10,
-                 shrinkage=0.01,
-                 interaction.depth = 4,
-                 cv.fold = 10)
+                 n.trees = 10000,
+                 shrinkage = 0.01,
+                 interaction.depth = 2,
+                 cv.folds = 10,
+                 verbose = TRUE)
 
-boost.fit <- gbm(y ~ ., data = train_set,
-                 distribution = "bernoulli", n.trees = 10,
+# Cross-validation to select the best number of boosts(trees)
+n_trees_cv <- gbm.perf(boost_fit, method = 'cv', plot.it = T)
+
+gbmpred <- predict(boost_fit, newdata = test_set[,-1],
+                   n.trees = n_trees_cv, type = 'response')
+
+# Calculate test misclassification rate assuming threshold 0.5
+sum((gbmpred >= 0.5) != test_set[ ,1]) / nrow(test_set)
+
+
+# Fit boosted trees to the data with shrinkage = 0.001
+boost_fit_001 <- gbm(y ~ ., data = train_set,
+                 distribution = "bernoulli",
+                 n.trees = 10000,
                  shrinkage=0.001,
-                 interaction.depth = 4,
+                 interaction.depth = 2,
                  cv.fold = 10)
 
-n.trees.cv <- gbm.perf(boost.fit, method = "cv", oobag.curve = TRUE, overlay = TRUE)
+n_trees_cv_001 <- gbm.perf(boost_fit_001, method = 'cv')
 
-gbmpred <- predict(boost.fit, newdata = test_est,
-                   n.trees = n.trees.cv)
+gbmpred <- predict(boost_fit_001, newdata = test_est,
+                   n.trees = n_trees_cv_001)
 
-# Calculate the test error
+# Calculate the test misclassification rate
+gbmpred_001 <- predict(boost_fit_001, newdata = test_set[,-1],
+                   n.trees = n_trees_cv_001, type = 'response')
+
+# Calculate test misclassification rate assuming threshold 0.5
+sum((gbmpred_001 >= 0.5) != test_set[ ,1]) / nrow(test_set)
