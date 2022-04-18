df3001b83becb5b33682bdca432f1b50bcdbdd7e
Austgen <craustgen@tamu.edu>
2022-04-18 17:19:15 -0500

    update

--- boosted trees test.R
+++ boosted trees test.R
@@ -16,7 +16,7 @@ n <- length(dataset)
 set.seed(639)
 
 #Establish Possible Parameters
-
+sample_size=0.6
 interactionDepthVector = c(1, 2, 3, 4, 5)       # depth of the trees
 shrinkageVector = seq(0.001, 0.01, 0.005)         # learning rate
 bagFractionVector = seq(from = 0.2, to = 0.7, by = 0.1)          # number of obs. to use in training
@@ -43,7 +43,7 @@ bootstrapID <- bootstrapID + 1
 
 for (b in 1:bootStraps){
   
-  picked = sample(seq_len(nrow(dataset)),size = sample_size)
+  picked = sample(seq_len(nrow(dataset)),size = floor(sample_size*nrow(dataset)))  
   outerSplit<-dataset[picked,]
   innerSplit<-dataset[-picked,]
   
@@ -56,10 +56,6 @@ for (b in 1:bootStraps){
                           cv.fold=20)
           boost.inner.cvtrees<-gbm.perf(boost.inner, method = "cv")
           boost.outer.pred=predict(boost.inner,
-                                   newdata=outerSplit,
-                                   n.trees=boost.inner.cvtrees,
-                                   type='response')
-          boost.inner.pred=predict(boost.inner,
                                    newdata=outerSplit,
                                    n.trees=boost.inner.cvtrees,
                                    type='response')
@@ -79,7 +75,58 @@ resultsDataFrame
 
 summary = resultsDataFrame %>%
   group_by(bootstrapID) %>%
-  summarize(meanAccuracy = mean(validationAccuracy)) %>%
-  arrange(meanAccuracy, desc=TRUE)
+  summarize(meanAccuracy = mean(validationAccuracy), SD = sd(validationAccuracy)) %>%
+  arrange(meanAccuracy, desc=TRUE) %>%
+  print(n=60)
+
+
+
+
+
+names<- c('bootStrapNum','testAccuracy','validationAccuracy')
+resultsDataFrame <- data.frame(matrix(ncol = length(names), nrow = 0)) 
+colnames(resultsDataFrame) <- names
+
+
+bootStraps <- 20
+
+for (b in 1:bootStraps){
+  
+  picked = sample(seq_len(nrow(dataset)),size = floor(sample_size*nrow(dataset)))
+  outerSplit<-dataset[picked,]
+  innerSplit<-dataset[-picked,]
+  
+  boost.inner=gbm(y~.,data=innerSplit,
+                  distribution="bernoulli",
+                  n.trees=1000,
+                  shrinkage=0.006,
+                  interaction.depth=5,
+                  bag.fraction=0.6,
+                  cv.fold=10)
+  boost.inner.cvtrees<-gbm.perf(boost.inner, method = "cv")
+  boost.outer.pred=predict(boost.inner,
+                           newdata=outerSplit,
+                           n.trees=boost.inner.cvtrees,
+                           type='response')
+  boost.inner.pred=predict(boost.inner,
+                           newdata=innerSplit,
+                           n.trees=boost.inner.cvtrees,
+                           type='response')
+  
+  responseVector<-boost.outer.pred>=0.5
+  innerresponseVector<-boost.inner.pred>=0.5
+  accuracy<-(nrow(outerSplit)-sum(abs(responseVector-outerSplit$y)))/nrow(outerSplit)
+  inneraccuracy<-(nrow(innerSplit)-sum(abs(innerresponseVector-innerSplit$y)))/nrow(innerSplit)
+  strapResult<-c(b,inneraccuracy,accuracy)
+  resultsDataFrame<-rbind(resultsDataFrame,strapResult)
+  print(paste(b,'complete'))
+}
+
+newDataFrame<-read.csv('resultsDataFrame.csv')
+
+summary = newDataFrame %>%
+  group_by(bootstrapID) %>%
+  summarize(meanAccuracy = mean(validationAccuracy), SD = sd(validationAccuracy)) %>%
+  arrange(meanAccuracy, desc=TRUE) %>%
   print(n=60)
 
